67abee68b2b5c534c920ecf8cf505126589ba30e
MasaNakamura-ctrl <m.nakamura.contact@gmail.com>
2025-05-03 11:49:30 +0900

    Add:仮データの作成と反映

--- front/src/components/Tsutsumeki.vue
+++ front/src/components/Tsutsumeki.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { tsutsumekis } from './tsutsumeki.js'
+
 </script>
 <template>
     <h1>ツツメキ</h1>
@@ -7,14 +9,8 @@
         </textarea>
         <button>ツツメキ</button>
     </div>
-    <div class="Tsutsumeki-poted">
-        <p class="Tsutsumeki-words">投稿されたツツメキ</p>
-        <button>WaGotcha</button>
-        <button>改む</button>
-        <button>消つ</button>
-    </div>
-    <div class="Tsutsumeki-poted">
-        <p class="Tsutsumeki-words">投稿されたツツメキ</p>
+    <div class="Tsutsumeki-poted" v-for="(tsutsumeki, index) in tsutsumekis">
+        <p class="Tsutsumeki-words">{{ tsutsumeki.text }}</p>
         <button>WaGotcha</button>
         <button>改む</button>
         <button>消つ</button>
---
+++ front/src/components/tsutsumeki.js
@@ -0,0 +1,7 @@
+import { ref } from 'vue'
+
+export const tsutsumekis = ref([
+    {id:1, text: "六段の調べやりたい人いない？"},
+    {id:2, text: "来週演奏会やります！"},
+    {id:3, text: "○市でもくもく会やってませんか？"}
+])
