fcd89bc14f5bdd3a205f239a890f6ca3e2b35d50
MasaNakamura-ctrl <m.nakamura.contact@gmail.com>
2025-05-03 18:32:35 +0900

    Update:データ更新とWaGotchaの実装

--- front/src/components/Tsutsumeki.vue
+++ front/src/components/Tsutsumeki.vue
@@ -2,27 +2,46 @@
 import { ref } from 'vue'
 import { tsutsumekis } from './tsutsumeki.js'
 
-const inputText = ref('')
-// WaGotchaされたインデックスを記録(RESTAPIと連携次第別対応)
-const clickedIndices = ref(new Set())
-
 // データ追加
+const inputText = ref('')
+// 仮データの保持レコード数に1を加えたもの
+let id_count = 4;
 // IDへの対応はRESTAPIと連携次第そちらへ移行
 function addTsutsumeki(){
-    const tailId = tsutsumekis.value.length + 1
+    console.log(id_count)
     if (inputText.value.trim()) {
-        tsutsumekis.value.push({id:tailId, tsutsumeki: inputText.value })
+        tsutsumekis.value.push({id:id_count, tsutsumeki: inputText.value })
         inputText.value = ''
+        id_count += 1;
     }
+    console.log(id_count)
 }
+
+// データ更新
+let editingIndex = ref(null)
+const editingText = ref('')
+function editTsutsumeki(index){
+    editingIndex.value = index
+    editingText.value = tsutsumekis.value[index].tsutsumeki
+}
+function completeEdittingTsutsumeki(index){
+    editingIndex.value = null
+}
+function quitEdittingTsutsumeki(index){
+    tsutsumekis.value[index].tsutsumeki = editingText.value
+    editingIndex.value = null
+}
+
 // データ削除
 function deleteTsutsumeki(index){
     tsutsumekis.value.splice(index,1)
 }
 
 // WaGotcha押下(RESTAPIと連携次第別対応)
+// WaGotchaされたインデックスを記録(RESTAPIと連携次第別対応)
+const clickedIndices = ref(new Set())
 function clickWaGotcha(index){
-    clickedIndices.value.add(index)
+    clickedIndices.value.add(tsutsumekis.value[index]["id"])
 }
 </script>
 <template>
@@ -33,11 +52,19 @@ function clickWaGotcha(index){
         <button @click="addTsutsumeki">ツツメキ</button>
     </div>
     <div class="Tsutsumeki-posted" v-for="(tsutsumeki, index) in tsutsumekis">
-        <p class="Tsutsumeki-words">{{ tsutsumeki.tsutsumeki }}</p>
-        <button class="WaGotcha" @click="clickWaGotcha(index)"
-        :class= "{active: clickedIndices.has(index)}"
-        >WaGotcha</button>
-        <button>改む</button>
-        <button @click="deleteTsutsumeki(index)">消つ</button>
+        <div v-if="editingIndex !== index">
+            <p class="Tsutsumeki-words">{{ tsutsumeki.tsutsumeki }}</p>
+            <button class="WaGotcha" @click="clickWaGotcha(index)"
+            :class= "{active: clickedIndices.has(tsutsumeki.id)}"
+            >WaGotcha</button>
+            <button @click="editTsutsumeki(index)">改む</button>
+            <button @click="deleteTsutsumeki(index)">消つ</button>
+        </div>
+        <div v-else>
+            <textarea v-model="tsutsumekis[index].tsutsumeki" class="Tsutsumeki-text" maxlength="140"></textarea>
+            <button class="WaGotcha" @click="quitEdittingTsutsumeki(index)">WaGotcha</button>
+            <button @click="completeEdittingTsutsumeki(index)">記す</button>
+            <button @click="quitEdittingTsutsumeki(index)">辞む</button>
+        </div>
     </div>
 </template>
