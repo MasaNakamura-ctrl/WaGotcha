956fb25e789f0197a3479a7564f57d76ff1ecbc2
MasaNakamura-ctrl <m.nakamura.contact@gmail.com>
2025-05-03 10:55:35 +0900

    Update:ツツメキ画面の構成要素を仮実装

--- front/src/components/Tsutsumeki.vue
+++ front/src/components/Tsutsumeki.vue
@@ -2,4 +2,21 @@
 </script>
 <template>
     <h1>ツツメキ</h1>
+    <div class="Tsutsumeki-create">
+        <textarea class="Tsutsumeki-text" placeholder="ツツメキしてWaGotchaしてもらおう！">
+        </textarea>
+        <button>ツツメキ</button>
+    </div>
+    <div class="Tsutsumeki-poted">
+        <p class="Tsutsumeki-words">投稿されたツツメキ</p>
+        <button>WaGotcha</button>
+        <button>改む</button>
+        <button>消つ</button>
+    </div>
+    <div class="Tsutsumeki-poted">
+        <p class="Tsutsumeki-words">投稿されたツツメキ</p>
+        <button>WaGotcha</button>
+        <button>改む</button>
+        <button>消つ</button>
+    </div>
 </template>
