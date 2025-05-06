807c5ba57aa57f6a09f4cd88aeff21c646c450e6
MasaNakamura-ctrl <m.nakamura.contact@gmail.com>
2025-05-07 03:46:46 +0900

    Update:Docker上のDBとの接続を修正

--- api/src/main/java/com/example/api/tsutsumeki/Tsutsumeki.java
+++ api/src/main/java/com/example/api/tsutsumeki/Tsutsumeki.java
@@ -1,6 +1,14 @@
 package com.example.api.tsutsumeki;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Tsutsumeki {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String tsutsumeki;
     public int getId(){
