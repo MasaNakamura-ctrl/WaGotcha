e7a90dff8893bd64979d723c7bd8e2285572a6a1
MasaNakamura-ctrl <m.nakamura.contact@gmail.com>
2025-05-07 03:08:35 +0900

    Add:DTOと対応したControllerでの処理を実装

--- api/src/main/java/com/example/api/tsutsumeki/TsutsumekiController.java
+++ api/src/main/java/com/example/api/tsutsumeki/TsutsumekiController.java
@@ -1,16 +1,40 @@
 package com.example.api.tsutsumeki;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+
 
 @RestController
 @RequestMapping("/api/tsutsumekis")
 public class TsutsumekiController {
     @Autowired
     private TsutsumekiService tsutsumekiService;
+
+    @GetMapping
+    public List<Tsutsumeki> getTsutsumekis(){
+        return tsutsumekiService.getAllTsutsumekis();
+    }
+    
+    @PostMapping
+    public void createTsutsumeki(@RequestBody TsutsumekiRequest request) {
+        tsutsumekiService.postTsutsumeki(request.getTsutsumeki());
+    }
+
+    @PutMapping("/{id}")
+    public void updateTsutsumeki(@PathVariable int id, @RequestBody TsutsumekiRequest request) {
+        tsutsumekiService.putTsutsumeki(id, request.getTsutsumeki());
+    }
 
     @DeleteMapping("/{id}")
     public void deleteTsutsumeki(@PathVariable int id) {
---
+++ api/src/main/java/com/example/api/tsutsumeki/TsutsumekiRequest.java
@@ -0,0 +1,13 @@
+package com.example.api.tsutsumeki;
+
+public class TsutsumekiRequest {
+    private String tsutsumeki;
+
+    public String getTsutsumeki(){
+        return tsutsumeki;
+    }
+
+    public void setTsutsumeki(String tsutsumeki){
+        this.tsutsumeki = tsutsumeki;
+    }
+}
