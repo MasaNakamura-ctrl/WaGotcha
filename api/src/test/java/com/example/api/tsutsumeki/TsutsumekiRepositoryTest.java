f55d28b7cb3e1f3b208f9a07ad342691fbca39a1
MasaNakamura-ctrl <m.nakamura.contact@gmail.com>
2025-05-10 15:22:55 +0900

    Add:MockitoによるTsutsumekiRepositoryの単体テスト

---
+++ api/src/test/java/com/example/api/tsutsumeki/TsutsumekiRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.example.api.tsutsumeki;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class TsutsumekiRepositoryTest {
+    
+    @Mock
+    TsutsumekiRepository tsutsumekiRepository;
+
+    @Test
+    void testFindTsutsumeki_found(){
+        Tsutsumeki tsutsumeki = new Tsutsumeki(1, "Here I Am!");
+        when(tsutsumekiRepository.findById(1)).thenReturn(tsutsumeki);
+
+        Tsutsumeki result = tsutsumekiRepository.findById(1);
+        assertNotNull(result);
+        assertEquals("Here I Am!", result.getTsutsumeki());
+    }
+
+    @Test
+    void testFindTsutsumeki_notfound(){
+        when(tsutsumekiRepository.findById(2)).thenReturn(null);
+
+        Tsutsumeki result = tsutsumekiRepository.findById(2);
+        assertNull(result);
+    }
+}
