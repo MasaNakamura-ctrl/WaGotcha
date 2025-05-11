c59ec1dca9fc09e4fed4e174ace2c7211baa5ea9
MasaNakamura-ctrl <m.nakamura.contact@gmail.com>
2025-05-11 14:05:36 +0900

    Update:Postのテスト実施

--- api/src/test/java/com/example/api/tsutsumeki/TsutsumekiControllerTest.java
+++ api/src/test/java/com/example/api/tsutsumeki/TsutsumekiControllerTest.java
@@ -16,10 +16,9 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(TsutsumekiController.class)
@@ -35,28 +34,53 @@ public class TsutsumekiControllerTest {
 
     @Test
     void testGetTsutsumekis() throws Exception{
-    Tsutsumeki expected1 = new Tsutsumeki(1, "First");
-    Tsutsumeki expected2 = new Tsutsumeki(2,"Second");
-    List<Tsutsumeki> tsutsumekiList = Arrays.asList(expected1, expected2);
+        Tsutsumeki expected1 = new Tsutsumeki(1, "First");
+        Tsutsumeki expected2 = new Tsutsumeki(2,"Second");
+        List<Tsutsumeki> tsutsumekiList = Arrays.asList(expected1, expected2);
 
-    when(tsutsumekiService.getAllTsutsumekis()).thenReturn(tsutsumekiList);
+        when(tsutsumekiService.getAllTsutsumekis()).thenReturn(tsutsumekiList);
 
-    mockMvc.perform(get("/api/tsutsumekis")
-    .contentType(MediaType.APPLICATION_JSON))
-    .andExpect(status().isOk())
-    .andExpect(jsonPath("$.length()").value(2))
-    .andExpect(jsonPath("$[0].id").value(1))
-    .andExpect(jsonPath("$[0].tsutsumeki").value("First"))
-    .andExpect(jsonPath("$[1].id").value(2))
-    .andExpect(jsonPath("$[1].tsutsumeki").value("Second"));
+        mockMvc.perform(get("/api/tsutsumekis")
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.length()").value(2))
+        .andExpect(jsonPath("$[0].id").value(1))
+        .andExpect(jsonPath("$[0].tsutsumeki").value("First"))
+        .andExpect(jsonPath("$[1].id").value(2))
+        .andExpect(jsonPath("$[1].tsutsumeki").value("Second"));
     }
 
     @Test
     void testGetAllTsutsumekis_return500() throws Exception{
-    when(tsutsumekiService.getAllTsutsumekis()).thenThrow(new RuntimeException("Internal server error"));
-    mockMvc.perform(get("/api/tsutsumekis")
-    .contentType(MediaType.APPLICATION_JSON))
-    .andExpect(status().isInternalServerError())
-    .andExpect(jsonPath("$.error").value("Internal server error"));
+        when(tsutsumekiService.getAllTsutsumekis()).thenThrow(new RuntimeException("Internal server error"));
+        mockMvc.perform(get("/api/tsutsumekis")
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isInternalServerError())
+        .andExpect(jsonPath("$.error").value("Internal server error"));
+    }
+
+    @Test
+    void testCreateTsutsumeki() throws Exception{
+        TsutsumekiRequest request = new TsutsumekiRequest();
+        request.setTsutsumeki("posted");
+        Tsutsumeki posted = new Tsutsumeki(1, "posted");
+        when(tsutsumekiService.postTsutsumeki("posted")).thenReturn(posted);
+        mockMvc.perform(post("/api/tsutsumekis")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(request)))
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("$.id").value(1))
+        .andExpect(jsonPath("$.tsutsumeki").value("posted"));
+    }
+
+    @Test
+    void testCreateTsutsumekis_return500() throws Exception{
+        TsutsumekiRequest request = new TsutsumekiRequest();
+        request.setTsutsumeki("posted");
+        when(tsutsumekiService.postTsutsumeki("posted")).thenThrow(new RuntimeException("Internal server error"));
+        mockMvc.perform(post("/api/tsutsumekis")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(request)))
+        .andExpect(status().isInternalServerError());
     }
 }
