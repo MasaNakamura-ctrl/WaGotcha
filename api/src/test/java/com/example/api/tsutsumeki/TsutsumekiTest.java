ddda650454580b3ff36157ffa9d31c294e033310
MasaNakamura-ctrl <m.nakamura.contact@gmail.com>
2025-05-24 13:09:50 +0900

    Update:TsutsumekiTest.java

--- api/src/test/java/com/example/api/tsutsumeki/TsutsumekiTest.java
+++ api/src/test/java/com/example/api/tsutsumeki/TsutsumekiTest.java
@@ -1,5 +1,6 @@
 package com.example.api.tsutsumeki;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import org.junit.jupiter.api.Test;
 
 public class TsutsumekiTest {
@@ -11,5 +12,29 @@ public class TsutsumekiTest {
 
         assertEquals(1,req.getId());
         assertEquals("Success!",req.getTsutsumeki());
+    }
+
+    @Test
+    void testEqualAndHashSuccess(){
+        Tsutsumeki test1 = new Tsutsumeki(1, "Test");
+        Tsutsumeki test2 = new Tsutsumeki(1, "Test");
+
+        // equals
+        assertEquals(test1,test2);
+
+        // hashCode
+        assertEquals(test1.hashCode(),test2.hashCode());
+    }
+
+    @Test
+    void testEqualAndHashFailure(){
+        Tsutsumeki test1 = new Tsutsumeki(1, "Test");
+        Tsutsumeki test2 = new Tsutsumeki(2, "Test");
+
+        // equals
+        assertNotEquals(test1,test2);
+
+        // hashCode
+        assertNotEquals(test1.hashCode(),test2.hashCode());
     }
 }
